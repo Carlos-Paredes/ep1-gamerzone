18850679a04a001ee4079d52c29f78647bb1b470
Carlos-Paredes <carlosparedes130298@gmail.com>
2021-05-05 21:56:04 -0500

    agregado de img en asset

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainContentComponent } from './common/main-content/main-content.component';
 import { BreakingbadComponent } from './components/breakingbad/breakingbad.component';
 import { FifaComponent } from './components/fifa/fifa.component';
+import { FutbolistasComponent } from './locales/futbolistas/futbolistas.component';
 
 const routes: Routes = [
   {path: '', component: MainContentComponent},
   {path: 'fifa', component: FifaComponent},
-  {path: 'breakingbad', component: BreakingbadComponent}
+  {path: 'breakingbad', component: BreakingbadComponent},
+  {path: 'futbolistas', component: FutbolistasComponent}
 ];
 
 @NgModule({
--- src/app/entities/futbolistas.ts
+++ src/app/entities/futbolistas.ts
@@ -1,9 +1,9 @@
 import { Futbolista } from "./futbolista";
 export const FUTBOLISTAS: Futbolista[] = [
-    {id: 1, nombre: "Paolo Guerrero", posicion: "Delantero Central", nacionalidad: "Peru",edad: "38 años", foto: "assets/genios/peterparker.jpg"},
-    {id: 2, nombre: "Jefferson Farfán", posicion: "Extremo", nacionalidad: "Peru",edad: "37 años", foto: "assets/genios/peterparker.jpg"},
-    {id: 3, nombre: "Cristiano Ronaldo", posicion: "Extremo", nacionalidad: "Portugal",edad: "36 años", foto: "assets/genios/peterparker.jpg"},
-    {id: 4, nombre: "Lionel Messi", posicion: "Extremo", nacionalidad: "Peru",edad: "38 años", foto: "assets/genios/peterparker.jpg"},
-    {id: 5, nombre: "Kylliam Mbappé", posicion: "Delantero Central", nacionalidad: "Francia",edad: "23 años", foto: "assets/genios/peterparker.jpg"},
+    {id: 1, nombre: "Paolo Guerrero", posicion: "Delantero Central", nacionalidad: "Peru",edad: "38 años", foto: "assets/futbolistas/paoloGuerrero.jpg"},
+    {id: 2, nombre: "Jefferson Farfán", posicion: "Extremo", nacionalidad: "Peru",edad: "37 años", foto: "assets/futbolistas/jFarfan.png"},
+    {id: 3, nombre: "Cristiano Ronaldo", posicion: "Extremo", nacionalidad: "Portugal",edad: "36 años", foto: "assets/futbolistas/cr7.jpeg"},
+    {id: 4, nombre: "Lionel Messi", posicion: "Extremo", nacionalidad: "Peru",edad: "36 años", foto: "assets/futbolistas/messi.png"},
+    {id: 5, nombre: "Kylliam Mbappé", posicion: "Delantero Central", nacionalidad: "Francia",edad: "23 años", foto: "assets/futbolistas/kiliam.jpg"},
 
 ]
