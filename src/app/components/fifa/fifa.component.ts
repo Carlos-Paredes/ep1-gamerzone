76616692a3cd3f5a5ed1fb1530344da89b942c14
Christofer Yupanqui <cayr2016@gmail.com>
2021-05-23 14:46:35 -0500

    adding modal detail

--- src/app/components/fifa/fifa.component.ts
+++ src/app/components/fifa/fifa.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { Fifa } from 'src/app/entities/fifa';
-import { FifaService } from 'src/app/services/fifa.service';
+import {Component, OnInit} from '@angular/core';
+import {Fifa} from 'src/app/entities/fifa';
+import {FifaService} from 'src/app/services/fifa.service';
 
 @Component({
-  selector: 'app-fifa',
-  templateUrl: './fifa.component.html',
-  styleUrls: ['./fifa.component.css']
+    selector: 'app-fifa',
+    templateUrl: './fifa.component.html',
+    styleUrls: ['./fifa.component.css']
 })
 export class FifaComponent implements OnInit {
-  listaJugadores: Fifa[];
-  constructor(private fifaService: FifaService) { }
+    listaJugadores: Fifa[];
+    playerSelected: Fifa;
 
-  ngOnInit(): void {
-    this.fifaService.fifaSelect().subscribe(
-      (res: Fifa[]) =>{
-        this.listaJugadores = res;
-        console.log(res);
-      }
-    )
-  }
+    constructor(private fifaService: FifaService) {
+    }
+
+    ngOnInit(): void {
+        this.fifaService.fifaSelect().subscribe(
+            (res: Fifa[]) => {
+                this.listaJugadores = res;
+                console.log(res);
+            }
+        )
+    }
+
+    displaySelectedPlayer(fifa: Fifa): void {
+      this.playerSelected = fifa;
+    }
 }
